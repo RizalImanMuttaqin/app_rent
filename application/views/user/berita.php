5b319716ad2710900d62a071960e4a74e5e22c22
mqomarrudin74 <mqomarrudin74@gmail.com>
2020-03-07 12:21:30 +0700

    progress

--- application/views/user/berita.php
+++ application/views/user/berita.php
@@ -5,10 +5,10 @@
 			<section id="page-title" class="page-title-left" style="background-image: url('<?php echo base_url('assets/user_template/images/bgtittle.jpg'); ?>'); background-attachment: fixed;">
 
 				<div class="container clearfix">
-					<h1 style="color: white; text-shadow: 2px 2px 5px black;"><b>BERITA DESA</b></h1>
+					<h1 style="color: white; text-shadow: 2px 2px 5px black;"><b>Our Product</b></h1>
 					<ol class="breadcrumb">
 						<li class="breadcrumb-item"><a href="#"><b style="color: white">Home</b></a></li>
-						<li class="breadcrumb-item active" aria-current="page"><b style="color: #DFDFDFDF">Berita Desa</b></li>
+						<li class="breadcrumb-item active" aria-current="page"><b style="color: #DFDFDFDF">Product</b></li>
 					</ol>
 				</div>
 
@@ -21,28 +21,44 @@
 					<?php $this->load->view('_partials_user/_breaking_news'); ?>
 
 					<div class="container clearfix">
+						
+						
+						<div class="single-post nobottommargin">
+							<div class="clearfix">
+								<div class="col-lg-12 col-md-12 center" style="background-color:white; padding: 20px 0px 100px 0px;">
+										<div class="col_one_third">
+											<label for="category" style="float: left;">Category : </label>
+											<select type="text" id="category" name="category" value="" class="sm-form-control">
+												<option>Show All Product</option>
+												<option>Camera</option>
+												<option>Lens</option>
+											</select>
+										</div>
+										<div class="col_one_third">
+											<label for="category" style="float: left;">Search : </label>
+											<input type="text" id="telepon" name="passlogin" value="" class="sm-form-control" />
+										</div>
+										<div class="col_one_third col_last">
+											<!-- <label for="category" style="float: left;"></label> -->
+											<button class="btn btn-info" type="submit" style="float: right; margin-top: 30px; font-weight: bold;">Filter</button>
+										</div>
+								</div>
+							</div>
 
-						<div class="postcontent nobottommargin clearfix">
 
-							<div class="single-post nobottommargin">
-
+							<div class="clearfix">
 							<!-- Single Post
 								============================================= -->
 								<!-- Entry Title
 									============================================= -->
-									<div class="entry-title">
-										<h2>Berita Desa</h2>
-									</div><!-- .entry-title end -->
+									<!-- <div class="entry-title">
+										<h2>Our Product</h2>
+									</div> -->
+									<!-- .entry-title end -->
 
 								<!-- Entry Meta
 									============================================= -->
-									<ul class="entry-meta clearfix">
-										<!-- <div class="fancy-title title-border">
-											<h3>Kategori Berita Terbaru</h3>
-										</div> -->
-										<!-- <li><i class="icon-calendar3"></i> 10th July 2014</li> -->
-										<li><i class="icon-folder-open"></i> <a href="#">Berita Desa</a></li>
-									</ul><!-- .entry-meta end -->
+								
 									<?php foreach ($data as $kategori) : ?>
 									<div class="fancy-title title-border">
 										<h3>Kategori Berita <?php echo $kategori->nama_kategori; ?></h3>
@@ -79,7 +95,6 @@
 
 							</div><!-- .postcontent end -->
 
-							<?php $this->load->view('_partials_user/_sidebar_left'); ?>
 
 						</div>
 
